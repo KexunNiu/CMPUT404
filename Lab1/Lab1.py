f47cff09ac4a895d629a76b63c051945390fb229
KexunNiu <77284970+KexunNiu@users.noreply.github.com>
2022-09-13 05:48:03 +0800

    Update Lab1.py
    print raw url

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -6,3 +6,7 @@ print(requests.__version__)
 #GET https://www.google.com/
 getGoogle = requests.get("https://www.google.com/")
 print(getGoogle.status_code)
+
+#print the script from raw url itself
+getGitRaw = requests.get("https://raw.githubusercontent.com/KexunNiu/CMPUT404/main/Lab1/Lab1.py")
+print(getGitRaw.text)

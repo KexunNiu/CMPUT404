b447ae5031693104b10fd221b22e55580ed3c799
KexunNiu <77284970+KexunNiu@users.noreply.github.com>
2022-09-18 22:18:21 -0600

    update client.py

--- Lab2/client.py
+++ Lab2/client.py
@@ -1,2 +1,35 @@
+#Reference: https://drive.google.com/file/d/1Kvp7ZRIp2gseromIrwiWZpCXJsH3HkBn/view
+#Wrote these after watching the video
+
 import socket
 
+def main():
+
+    skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    url = "www.google.com"
+    payload = f'GET / HTTP/1.0\r\nHost: {url}\r\n\r\n'
+    buffersize = 4068
+
+    ip = socket.gethostbyname(url)
+    skt.connect((ip,80))
+
+    skt.sendall(payload.encode('utf-8'))
+
+    skt.shutdown(socket.SHUT_WR)
+
+    output = b""
+
+    while skt:
+        temp = skt.recv(buffersize)
+        if not temp:
+            break
+        output += temp
+    
+    print(output)
+
+    skt.close()
+
+
+if __name__=="__main__":
+    main()
